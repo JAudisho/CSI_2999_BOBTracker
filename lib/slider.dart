12f70c891ad5dd0be4f17c3b53e12d7f67ff8336
TheCzar27 <Lucasczar27@gmail.com>
2023-03-09 02:14:35 -0500

    New Commit

--- lib/slider.dart
+++ lib/slider.dart
@@ -52,7 +52,7 @@ class _HomepageState extends State<Homepage> {
                                     offset: Offset(4, 4))
                               ]),
                           child: Text(
-                            'Flutter Carousel Slider item ${i + 1}',
+                            'Carousel Slider item ${i + 1}',
                             style: TextStyle(color: Colors.white, fontSize: 15),
                           ),
                         )
